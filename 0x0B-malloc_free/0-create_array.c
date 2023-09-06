a6555834af32df7d065d7228e33417a67116ba7b
haddani12 <haddani.zmr2017@gmail.com>
2023-09-06 08:48:01 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-<<<<<<< HEAD
  * create_array - create array of size size and assign char c
  * @size: size of array
  * @c: char to assign
@@ -21,25 +20,4 @@ char *create_array(unsigned int size, char c)
 	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
-=======
- *  * create_array - create array of size size and assign char c
- *   * @size: size of array
- *    * @c: char to assign
- *     * Description: create array of size size and assign char c
- *      * Return: pointer to array, NULL if fail
- *       *
- *        */
-char *create_array(unsigned int size, char c)
-{
-		char *str;
-			unsigned int i;
-
-				str = malloc(sizeof(char) * size);
-					if (size == 0 || str == NULL)
-								return (NULL);
-
-						for (i = 0; i < size; i++)
-									str[i] = c;
-							return (str);
->>>>>>> c4bfd92574fb6e4031cec3ed5d363adf4bd0521e
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-<<<<<<< HEAD
  * free_grid - frees 2d array
  * @grid: 2d grid
  * @height: height dimension of grid
@@ -19,22 +18,4 @@ void free_grid(int **grid, int height)
 		free(grid[i]);
 	}
 	free(grid);
-=======
- *  * free_grid - frees 2d array
- *   * @grid: 2d grid
- *    * @height: height dimension of grid
- *     * Description: frees memory of grid
- *      * Return: nothing
- *       *
- *        */
-void free_grid(int **grid, int height)
-{
-		int i;
-
-			for (i = 0; i < height; i++)
-					{
-								free(grid[i]);
-									}
-				free(grid);
->>>>>>> c4bfd92574fb6e4031cec3ed5d363adf4bd0521e
 }
